f612845d4e3203430b06ded95f5023fdba638cc1
Thomas Graf <tgraf@Thomass-MBP.fritz.box>
2021-05-06 08:23:34 +0200

    modified script

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re")})();
+//#(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re")})();
 //# sourceMappingURL=script.js.map
